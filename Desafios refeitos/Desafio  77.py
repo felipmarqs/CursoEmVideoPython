c84aa3d7432d1e8238e8279143fa780b422b92ab
felipmarqs <39883697+felipmarqs@users.noreply.github.com>
2018-10-27 17:17:08 -0300

    Update Desafio  77.py

--- Desafios refeitos/Desafio  77.py
+++ Desafios refeitos/Desafio  77.py
@@ -1,8 +1,9 @@
+print('Detector de vogais em uma Tupla')
 palavras = ('aprender','programar','linguagem','python','curso','gratis','estudar',
             'praticar','trabalhar','mercado','programador','futuro')
 cont = 0
 for p in palavras:
-    print(f'\nNa palavra {p.upper()} temos', end=' ')
+    print(f'\nNa palavra {p.upper()} temos:', end=' ')
     for letra in p:
         if letra.lower() in 'aeiou':
             print(letra,end=' ')
